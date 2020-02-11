febab15f157dbaa36bd9201047a0186c611862a5
noble0423 <ryan.noble82@gmail.com>
2020-02-11 10:53:24 -0600

    changed the structure of const quiz from an object of objects to an array of objects in order to use an index variable to cycle through my questions/answers. Code is functioning when using hard-coded index for const index. I'll add logic later that will increment that index variable during the checkAnswers().

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // VARIABLES
 //=========================================================================================================================================
 // Countdown timer
-let countdownTimer = 100;
+let countdownTimer = 60;
+
+// Index to track question we are on
+let questionIndex = 0;
 
 // HTML tie-ins
 const $startQuizBtn = document.getElementById("startQuiz");
+const $countdownTimerEl = document.getElementById("countdownTimer");
 const $questionP = document.getElementById("question");
 const $answerChoice1 = document.getElementById("answerChoice1");
 const $answerChoice2 = document.getElementById("answerChoice2");
@@ -12,58 +16,68 @@ const $answerChoice3 = document.getElementById("answerChoice3");
 const $answerChoice4 = document.getElementById("answerChoice4");
 
 // Var = Quiz Object that includes questions, corresponding answer choices, and the correct answer
-const quiz = {
-    question1: {
-        question: "question 1",
-        answerChoices: [
-            "question 1, answer choice 1", 
-            "question 1, answer choice 2", 
-            "question 1, answer choice 3", 
-            "question 1, answer choice 4"
-        ],
-        correctAnswer: "",
+const quiz = [
+    {
+        // question1: {
+            question: "question 1",
+            answerChoices: [
+                "question 1, answer choice 1", 
+                "question 1, answer choice 2", 
+                "question 1, answer choice 3", 
+                "question 1, answer choice 4"
+            ],
+            correctAnswer: "",
+        // },
     },
-    question2: {
-        question: "question 2",
-        answerChoices: [
-            "question 2, answer choice 1", 
-            "question 2, answer choice 2", 
-            "question 2, answer choice 3", 
-            "question 2, answer choice 4"
-        ],
-        correctAnswer: "",
+    {
+        // question2: {
+            question: "question 2",
+            answerChoices: [
+                "question 2, answer choice 1", 
+                "question 2, answer choice 2", 
+                "question 2, answer choice 3", 
+                "question 2, answer choice 4"
+            ],
+            correctAnswer: "",
+        // },
     },
-    question3: {
-        question: "question 3",
-        answerChoices: [
-            "question 3, answer choice 1", 
-            "question 3, answer choice 2", 
-            "question 3, answer choice 3", 
-            "question 3, answer choice 4"
-        ],
-        correctAnswer: "",
+    {
+        // question3: {
+            question: "question 3",
+            answerChoices: [
+                "question 3, answer choice 1", 
+                "question 3, answer choice 2", 
+                "question 3, answer choice 3", 
+                "question 3, answer choice 4"
+            ],
+            correctAnswer: "",
+        // },
     },
-    question4: {
-        question: "question 4",
-        answerChoices: [
-            "question 4, answer choice 1", 
-            "question 4, answer choice 2", 
-            "question 4, answer choice 3", 
-            "question 4, answer choice 4"
-        ],
-        correctAnswer: "",
+    {
+        // question4: {
+            question: "question 4",
+            answerChoices: [
+                "question 4, answer choice 1", 
+                "question 4, answer choice 2", 
+                "question 4, answer choice 3", 
+                "question 4, answer choice 4"
+            ],
+            correctAnswer: "",
+        // },
     },
-    question5: {
-        question: "question 5",
-        answerChoices: [
-            "question 5, answer choice 1", 
-            "question 5, answer choice 2", 
-            "question 5, answer choice 3", 
-            "question 5, answer choice 4"
-        ],
-        correctAnswer: "",
-    }
-};
+    {
+        // question5: {
+            question: "question 5",
+            answerChoices: [
+                "question 5, answer choice 1", 
+                "question 5, answer choice 2", 
+                "question 5, answer choice 3", 
+                "question 5, answer choice 4"
+            ],
+            correctAnswer: "",
+        // },
+    },
+];
 
 
 
@@ -84,29 +98,59 @@ const quiz = {
 //=========================================================================================================================================
 // Start quiz function
 function startQuizGame() {
+    // Timer starts
+    runCountdownTimer();
 
+    // render trivia function call
+    renderTrivia(questionIndex);
+};
+
+// Countdown Timer function
+function runCountdownTimer() {
+    var timerInterval = setInterval(function() {
+        countdownTimer--;
+        $countdownTimerEl.textContent = countdownTimer;
+
+        if (countdownTimer <= 0) {
+            clearInterval(timerInterval);
+
+            // possibly show a gif
+            // run displayQuizStats function
+            displayQuizStats();
+        }
+    }, 1000);
 };
 
 // Render questions and answer function
-function renderTrivia() {
+// TODO: change to accept an arg that will cycle through quiz object
+function renderTrivia(whatQuestionAreWeOn) {
+
+    console.log(whatQuestionAreWeOn);
+    console.log(quiz[whatQuestionAreWeOn]);
+
+
     $questionP.textContent = "";
-    $questionP.textContent = quiz.question1.question;
+    $questionP.textContent = quiz[whatQuestionAreWeOn].question;
 
     $answerChoice1.textContent = "";
-    $answerChoice1.textContent = quiz.question1.answerChoices[0];
+    $answerChoice1.textContent = quiz[whatQuestionAreWeOn].answerChoices[0];
 
     $answerChoice2.textContent = "";
-    $answerChoice2.textContent = quiz.question1.answerChoices[1];
+    $answerChoice2.textContent = quiz[whatQuestionAreWeOn].answerChoices[1];
 
     $answerChoice3.textContent = "";
-    $answerChoice3.textContent = quiz.question1.answerChoices[2];
+    $answerChoice3.textContent = quiz[whatQuestionAreWeOn].answerChoices[2];
 
     $answerChoice4.textContent = "";
-    $answerChoice4.textContent = quiz.question1.answerChoices[3];
+    $answerChoice4.textContent = quiz[whatQuestionAreWeOn].answerChoices[3];
 };
 
 // Check answer function
+
 // Display quiz stats function
+function displayQuizStats() {
+
+};
 
 
 
@@ -130,7 +174,10 @@ function renderTrivia() {
 $startQuizBtn.addEventListener("click", function(event) {
     event.preventDefault();
     // start quiz function call
+    startQuizGame();
 
     // render trivia function call
-    renderTrivia();
+    // renderTrivia();
 });
+
+// Button click listeners to grab the value of the answer button using data attribute
